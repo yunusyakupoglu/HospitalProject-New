e5548c50d263f7f8e78ccbc9b521f6fb828b8566
yunusyakupoglu <yunusyakupoglu@outlook.com>
2023-01-16 13:20:40 +0300

    last

--- DAL/ApplicationDbContext.cs
+++ DAL/ApplicationDbContext.cs
@@ -24,6 +24,7 @@ namespace DAL
         {
             var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             optionsBuilder.UseSqlite($"Data Source={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "project.db")}");
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
